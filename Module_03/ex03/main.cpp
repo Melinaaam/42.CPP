6cbdbbc911ad406f1dd43872a62b1c4bc2f5d364
melinaaam <mmo.melina@gmail.com>
2025-04-01 18:21:03 +0200

    en cour

--- Module_03/ex03/main.cpp
+++ Module_03/ex03/main.cpp
@@ -5,38 +5,6 @@
 
 int main()
 {
-    // std::cout << "******* Test de ClapTrap *******" << std::endl;
-    // ClapTrap David("David");
-    // ClapTrap Goliath("Goliath");
-    // ClapTrap cpyGoliath(Goliath);
-    // std::cout << std::endl;
-    // David.attack("Goliath");
-    // David.takeDamage(10);
-    // David.beRepaired(5);
-    // std::cout << std::endl;
-        
-    // std::cout << "******* Test de ScavTrap *******" << std::endl;
-    // ScavTrap Zeus("Zeus");
-    // ScavTrap Kronos("Kronos");
-    // ClapTrap cpyKronos(Kronos);
-    // std::cout << std::endl;
-    // Zeus.attack("Kronos");
-    // Kronos.takeDamage(20);
-    // Kronos.beRepaired(10);
-    // Zeus.guardGate();
-
-    // std::cout << std::endl;
-
-    // std::cout << "******* Test de FragTrap *******" << std::endl;
-    // FragTrap Athena("Athena");
-    // FragTrap Ulysse("Ulysse");
-    // FragTrap cpyUlysse(Ulysse);
-    // std::cout << std::endl;
-    // Athena.attack("Ulysse");
-    // Ulysse.takeDamage(20);
-    // Ulysse.beRepaired(10);
-    // Athena.highFivesGuys();
-
     std::cout << "******* Test de DiamondTrap *******" << std::endl << std::endl;
     DiamondTrap Hera("Hera");
     std::cout << std::endl;
