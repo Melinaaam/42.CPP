d6b4c2445ab121cedc04556339ac7888dcf736e5
Melinaaam <mmo.melina@gmail.com>
2025-04-30 18:38:14 +0200

    beggin ex00

---
+++ Module_09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,48 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include<iostream>
+#include<map>
+#include<string>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <stdexcept>
+
+class BitcoinExchange {
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange& copy);
+	BitcoinExchange& operator=(const BitcoinExchange& other);
+	~BitcoinExchange();
+
+	BitcoinExchange(const std::string& file);
+	float getpriceByDate(const std::string& date) const;
+
+
+	class FailOpenFile: public std::exception {
+		public:
+			virtual const char *what() const throw();
+	};
+
+private:
+	std::map<std::string, float> _priceByDate;
+	void loadDatabase(const std::string& filename);
+
+};
+
+#define BLACK   "\033[0;30m"
+#define RED     "\033[0;31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[38;5;220m"
+#define BLUE    "\033[0;34m"
+#define MAGENTA "\033[0;35;1m"
+#define MAGENTA1 "\033[0;35;2m"
+#define MAGENTA2 "\033[0;35;4m"
+#define CYAN    "\033[38;5;45m"
+#define PINK    "\033[38;5;213m"
+#define BOLD    "\033[1m"
+#define RESET   "\033[0m"
+#define CLIGNOTE "\033[0;35;5m"
+#define UNDER_	"\x1b[4m"
+#endif
---
+++ Module_09/ex00/srcs/BitcoinExchange.cpp
@@ -0,0 +1,58 @@
+#include"BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) {*this = copy;}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
+{
+	if (this != &other)
+		this->_priceByDate = other._priceByDate;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange(){}
+
+static bool isValidDateFormat(const std::string& date) {
+	if (date.length() != 10)
+		return false;
+	if (date[4] != '-' || date[7] != '-')
+		return false;
+	for (size_t i = 0; i < date.length(); ++i) {
+		if (i == 4 || i == 7)
+			continue;
+		if (!std::isdigit(date[i]))
+			return false;
+	}
+	return true;
+}
+
+BitcoinExchange::BitcoinExchange(const std::string& file) {loadDatabase(file);}
+
+
+float BitcoinExchange::getpriceByDate(const std::string& date) const
+{
+
+}
+
+void BitcoinExchange::loadDatabase(const std::string& filename)
+{
+	std::ifstream file(filename.c_str());
+	if (!file.is_open())
+		throw FailOpenFile();
+	std::string line;
+	std::getline(file, line);
+	while(std::getline(file, line)) {
+		std::istringstream ss(line);
+		std::string date, valueStr;
+		float value;
+		if (std::getline(ss, date, ',') && std::getline(ss, valueStr)){
+			if (!isValidDateFormat(date))
+				continue;
+			value = static_cast<float>(atof(valueStr.c_str()));
+			_priceByDate[date] = value;
+		}
+	}
+	file.close();
+}
+std::map<std::string, float> _priceByDate;
